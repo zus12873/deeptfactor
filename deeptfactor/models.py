7f1bcb451a38102ad1768be39666508f3288acb5
GIBAE KIM <anlito@kaist.ac.kr>
2020-11-10 02:36:50 +0900

    revision: network architecture changed

--- deeptfactor/models.py
+++ deeptfactor/models.py
@@ -3,10 +3,11 @@ import torch
 import torch.nn as nn
 
 class DeepTFactor(nn.Module):
-    def __init__(self, out_features):
+    def __init__(self, out_features=[0]):
         super(DeepTFactor, self).__init__()
         self.explainECs = out_features
-        self.cnn0 = CNN0()
+        self.layer_info = [[4, 4, 16], [12, 8, 4], [16, 4, 4]]
+        self.cnn0 = CNN(self.layer_info)
         self.fc1 = nn.Linear(in_features=128*3, out_features=512)
         self.bn1 = nn.BatchNorm1d(num_features=512)
         self.fc2 = nn.Linear(in_features=512, out_features=len(out_features))
@@ -32,48 +33,66 @@ class DeepTFactor(nn.Module):
         return x
 
 
-class CNN0(nn.Module):
+class CNN(nn.Module):
     '''
     Use second level convolution.
     channel size: 4 -> 16 
                   8 -> 12
                   16 -> 4
     '''
-    def __init__(self):
-        super(CNN0, self).__init__()
+    def __init__(self, layer_info):
+        super(CNN, self).__init__()
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.1)
-           
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(4,21))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(8,21))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(16,21))
 
-        self.batchnorm1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3 = nn.BatchNorm2d(num_features=128)
-
-        self.conv1_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(16,1))
-        self.conv2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(12,1))
-        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(4,1))
-
-        self.batchnorm1_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm2_1 = nn.BatchNorm2d(num_features=128)
-        self.batchnorm3_1 = nn.BatchNorm2d(num_features=128)
-
-        self.conv = nn.Conv2d(in_channels=128*3, out_channels=128*3, kernel_size=(1,1))
+        self.layers = nn.ModuleList()
+        pooling_sizes = []
+        for subnetwork in layer_info:
+            pooling_size = 0
+            self.layers += [self.make_subnetwork(subnetwork)]
+            for kernel in subnetwork:
+                pooling_size += (-kernel + 1)
+            pooling_sizes.append(pooling_size)
+              
+        if len(set(pooling_sizes)) != 1:
+            raise "Different kernel sizes between subnetworks"
+        pooling_size = pooling_sizes[0]
+        num_subnetwork = len(layer_info)
+        
+        self.conv = nn.Conv2d(in_channels=128*num_subnetwork, out_channels=128*3, kernel_size=(1,1))
         self.batchnorm = nn.BatchNorm2d(num_features=128*3)
-        self.pool = nn.MaxPool2d(kernel_size=(982,1), stride=1)
+        self.pool = nn.MaxPool2d(kernel_size=(1000+pooling_size,1), stride=1)
+
+
+    def make_subnetwork(self, subnetwork):
+        subnetworks = []
+        for i, kernel in enumerate(subnetwork):
+            if i == 0:
+                subnetworks.append(
+                    nn.Sequential(
+                        nn.Conv2d(in_channels=1, out_channels=128, kernel_size=(kernel, 21)),
+                        nn.BatchNorm2d(num_features=128),
+                        nn.ReLU(),
+                        nn.Dropout(p=0.1)
+                    )
+                )
+            else:
+                subnetworks.append(
+                    nn.Sequential(
+                        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(kernel, 1)),
+                        nn.BatchNorm2d(num_features=128),
+                        nn.ReLU(),
+                        nn.Dropout(p=0.1)
+                    )
+                )
+        return nn.Sequential(*subnetworks)
 
         
     def forward(self, x):
-        x1 = self.dropout(self.relu(self.batchnorm1(self.conv1(x))))
-        x2 = self.dropout(self.relu(self.batchnorm2(self.conv2(x))))
-        x3 = self.dropout(self.relu(self.batchnorm3(self.conv3(x))))
-        x1 = self.dropout(self.relu(self.batchnorm1_1(self.conv1_1(x1))))
-        x2 = self.dropout(self.relu(self.batchnorm2_1(self.conv2_1(x2))))
-        x3 = self.dropout(self.relu(self.batchnorm3_1(self.conv3_1(x3))))
-
-        x = torch.cat((x1, x2, x3), dim=1)
+        xs = []
+        for layer in self.layers:
+            xs.append(layer(x))
+        x = torch.cat(xs, dim=1)
         x = self.relu(self.batchnorm(self.conv(x)))
         x = self.pool(x)
         return x
